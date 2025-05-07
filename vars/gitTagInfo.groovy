6495b0bcd983056725c66d57cf12f876f7945f4b
Safae-ohm <safae.azouzou@ohm-energie.com>
2025-05-07 15:14:55 +0100

    Update gitTagInfo.groovy

--- vars/gitTagInfo.groovy
+++ vars/gitTagInfo.groovy
@@ -2,4 +2,5 @@ def call() {
     def tag = sh(script: 'git describe --tags --abbrev=0 || echo "no-tag"', returnStdout: true).trim()
     env.GIT_LAST_TAG = tag
     echo "Dernier tag Git trouvé : ${env.GIT_LAST_TAG}"
+    return tag
 }
