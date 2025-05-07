f0068f620482e384b0b1f7649eb281d10c083fee
Safae-ohm <safae.azouzou@ohm-energie.com>
2025-05-07 13:02:51 +0100

    Create rapportNotification.groovy

---
+++ vars/rapportNotification.groovy
@@ -0,0 +1,31 @@
+def call(Map params = [:]) {
+    
+    def attachments = params.get('attachmentsPattern', '')
+    def buildNumber = env.BUILD_NUMBER
+    def jobName = env.JOB_NAME
+    def buildUrl = env.BUILD_URL
+
+    // Charger le template
+    def rawTpl = libraryResource('notification/rapportnotify.tpl')
+
+    // Préparer les données à injecter dans le template
+    def engine = new groovy.text.SimpleTemplateEngine()
+    def binding = [
+        BUILD_NUMBER: buildNumber,
+        JOB_NAME    : jobName,
+        BUILD_URL   : buildUrl,
+        REPORTS     : attachments?.split(',')*.trim()
+    ]
+
+    def body = engine.createTemplate(rawTpl).make(binding).toString()
+
+    emailext(
+        subject: "Rapport de build : ${jobName} #${buildNumber}",
+        body: body,
+        mimeType: 'text/html',
+        to: recipient,
+        attachLog: true,
+        compressLog: true,
+        attachmentsPattern: attachments
+    )
+}
