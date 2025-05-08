c5c95648d3086958144a41f525eb80f937a35b13
Safae-ohm <safae.azouzou@ohm-energie.com>
2025-05-08 17:38:44 +0100

    Update CodeTesting.groovy

--- vars/CodeTesting.groovy
+++ vars/CodeTesting.groovy
@@ -1,7 +1,7 @@
 def call() {    
 
          def install = sh(script: 'npm install --no-audit') 
-         def dependencycheck= sh('npm audit --audit-level=critical')
+         def dependencycheck= sh(script: 'npm audit --audit-level=critical')
       }
 
 
